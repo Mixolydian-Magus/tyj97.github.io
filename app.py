b95d11052850122ca4c8eec797176271eb61943f
LAPTOP-L055RSK6\tyj97 <tyler.y.james@gmail>
2017-04-26 04:07:45 -0500

    fixed flask implementation, for real this time

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask # Import Flask package
-from flask import render_template
-from flask import url_for# Import render_template function
+from flask import render_template # Import render_template function
 app = Flask(__name__) # Construct Flask object named 'app'
 
 '''
@@ -17,10 +16,6 @@ URLs that will call the index() function if running app.py on localhost:
 @app.route('/index') # Secondary URL for function
 def index():
 	return render_template('index.html') # located in templates/
-
-@app.route('/base')
-def base():
-        return render_template('base.html')
 	
 @app.route('/about')
 def about():
@@ -29,62 +24,62 @@ def about():
 @app.route('/comics')
 def comics():
 	return render_template('comics.html') # Example of argument passing to HTML template
-	
+
 @app.route('/authors')
 def authors():
 	return render_template('authors.html')
-	
+
 @app.route('/genres')
 def genres():
 	return render_template('genres.html')
-			
+
+@app.route('/Katie_Tiedrich')
+def Katie_Tiedrich():
+	return render_template('Katie Tiedrich.html')
+
+@app.route('/Tim_Buckley')
+def Tim_Buckley():
+	return render_template('Tim Buckley.html')
+
+@app.route('/Zack Morrison')
+def Zack_Morrison():
+	return render_template('Zack Morrison.html')
+
+@app.route('/Awkward_Zombie')
+def Awkward_Zombie():
+	return render_template('awkward zombie.html')
+
+@app.route('/Paranatural')
+def Paranatural():
+	return render_template('paranatural.html')
+
+@app.route('/Ctrl+Alt+Del')
+def CtrlAltDel():
+	return render_template('Ctrl+Alt+Del.html')
+
 @app.route('/action')
 def action():
 	return render_template('action.html')
-		
-@app.route('/awkwardzombie')
-def awkwardzombie():
-	return render_template('awkwardzombie.html')
-		
+
 @app.route('/comedy')
 def comedy():
 	return render_template('comedy.html')
-		
-@app.route('/Ctrl+Alt+Del')
-def ctrl_alt_del():
-	return render_template('Ctrl+Alt+Del.html')
-		
+
 @app.route('/fantasy')
 def fantasy():
 	return render_template('fantasy.html')
-	
+
 @app.route('/gag-a-day')
-def gag_a_day():
+def gagaday():
 	return render_template('gag-a-day.html')
-		
-@app.route('/Katie Tiedrich')
-def katie_tiedrich():
-	return render_template('Katie Tiedrich.html')
-		
-@app.route('/paranatural')
-def paranatural():
-	return render_template('paranatural.html')
-	
+
 @app.route('/parody')
 def parody():
 	return render_template('parody.html')
-		
-@app.route('/slice of life')
+
+@app.route('/slice_of_life')
 def slice_of_life():
 	return render_template('slice of life.html')
-		
-@app.route('/Tim Buckley')
-def tim_buckley():
-	return render_template('Tim Buckley.html')
-		
-@app.route('/Zack Morrison')
-def zack_morrison():
-	return render_template('Zack Morrison.html')
-	
+
 if __name__ == '__main__':
 	app.run() # Run application
